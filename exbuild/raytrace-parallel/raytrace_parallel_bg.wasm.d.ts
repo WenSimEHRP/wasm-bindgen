99dbd6e25a398518c36e4c8780eda42c6e651579
guybedford <guybedford@gmail.com>
2026-03-08 19:28:45 +0000

    Deploying to gh-pages from @ wasm-bindgen/wasm-bindgen@6b6aa1ca4732bfd8f2910745e32ba959c094c2ad 🚀

---
+++ exbuild/raytrace-parallel/raytrace_parallel_bg.wasm.d.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export const __wbg_workerpool_free: (a: number, b: number) => void;
+export const child_entry_point: (a: number) => [number, number];
+export const workerpool_new: (a: number) => [number, number, number];
+export const __wbg_renderingscene_free: (a: number, b: number) => void;
+export const __wbg_scene_free: (a: number, b: number) => void;
+export const renderingscene_imageSoFar: (a: number) => any;
+export const renderingscene_promise: (a: number) => any;
+export const scene_new: (a: any) => [number, number, number];
+export const scene_render: (a: number, b: number, c: number) => [number, number, number];
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___wasm_bindgen_657deeddaa6cd8ac___JsValue__core_794a70210655af6c___result__Result_____wasm_bindgen_657deeddaa6cd8ac___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___js_sys_abe58c0587ae9284___Function_fn_wasm_bindgen_657deeddaa6cd8ac___JsValue_____wasm_bindgen_657deeddaa6cd8ac___sys__Undefined___js_sys_abe58c0587ae9284___Function_fn_wasm_bindgen_657deeddaa6cd8ac___JsValue_____wasm_bindgen_657deeddaa6cd8ac___sys__Undefined_______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___wasm_bindgen_657deeddaa6cd8ac___JsValue______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___wasm_bindgen_657deeddaa6cd8ac___JsValue______true__2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___web_sys_bb87943fc0d33771___features__gen_MessageEvent__MessageEvent______true_: (a: number, b: number, c: any) => void;
+export const memory: WebAssembly.Memory;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_thread_destroy: (a?: number, b?: number, c?: number) => void;
+export const __wbindgen_start: (a: number) => void;
---
+++ exbuild/wasm-audio-worklet/wasm_audio_worklet_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const __wbg_wasmaudioprocessor_free: (a: number, b: number) => void;
+export const wasmaudioprocessor_pack: (a: number) => number;
+export const wasmaudioprocessor_process: (a: number, b: number, c: number, d: any) => number;
+export const wasmaudioprocessor_unpack: (a: number) => number;
+export const web_main: () => any;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___wasm_bindgen_657deeddaa6cd8ac___JsValue__core_794a70210655af6c___result__Result_____wasm_bindgen_657deeddaa6cd8ac___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___js_sys_abe58c0587ae9284___Function_fn_wasm_bindgen_657deeddaa6cd8ac___JsValue_____wasm_bindgen_657deeddaa6cd8ac___sys__Undefined___js_sys_abe58c0587ae9284___Function_fn_wasm_bindgen_657deeddaa6cd8ac___JsValue_____wasm_bindgen_657deeddaa6cd8ac___sys__Undefined_______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___wasm_bindgen_657deeddaa6cd8ac___JsValue______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___wasm_bindgen_657deeddaa6cd8ac___JsValue______true__2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_657deeddaa6cd8ac___convert__closures_____invoke___web_sys_4ab954a45221c269___features__gen_MessageEvent__MessageEvent______true_: (a: number, b: number, c: any) => void;
+export const memory: WebAssembly.Memory;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_thread_destroy: (a?: number, b?: number, c?: number) => void;
+export const __wbindgen_start: (a: number) => void;
